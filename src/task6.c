a8095adc3a462a7e17cfced3d129d13a46155271
donus1perdonus <ekzozeta@gmail.com>
2025-09-28 22:17:41 +0300

    add task6

--- src/task6.c
+++ src/task6.c
@@ -1,7 +1,36 @@
 #include "task6.h"
+#include <stdio.h>
+#include <unistd.h>
 
 void task6_main(void) {
-    printf("=== Задание 6 ===\n");
-    printf("Реализация задания 6\n");
-    // Здесь будет код для задания 6
+    printf("=== Задание 6.1 ===\n");
+    printf("Реализация задания 6.1\n");
+    
+    int pid;
+    pid = fork();
+    printf("%d\n", pid);
+    
+    printf("=== Задание 6.2 ===\n");
+    printf("Реализация задания 6.2\n");
+
+    fork();
+    printf("Hi\n");
+    fork();
+    printf("Hi\n");
+
+    printf("=== Задание 6.3 ===\n");
+    printf("Реализация задания 6.3\n");
+
+    if (fork() || fork())
+        fork();
+    printf("forked! %d", fork());
+
+    printf("=== Задание 6.4 ===\n");
+    printf("Реализация задания 6.4\n");
+
+    if (fork() && (!fork())) {
+        if (fork() || fork())
+            fork();
+    }
+    printf("52\n");
 }
